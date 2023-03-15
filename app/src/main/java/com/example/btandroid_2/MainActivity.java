38bf3f13290dda517cbabce78d6a9c753c9d6734
tangquochung <19521583@gm.uit.edu.vn>
2023-03-15 20:36:14 +0700

    Up

---
+++ app/src/main/java/com/example/btandroid_2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.btandroid_2;
+
+import  androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+import android.net.Uri;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    final Button callButton = (Button) findViewById(R.id.callButton);
+    final EditText phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+
+    callButton.setOnClickListener(new OnClickListener() {
+        public void onClick(View v){
+            Intent callIntent = new Intent(Intent.ACTION_CALL,Uri.parse("tell:"+phoneNumber.getText()));
+            callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(callIntent);
+        }
+    });
+}
